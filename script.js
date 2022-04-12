59d2d92503c3f470fcc1b806efe228de84b7146c
RoxanneLefebvre <102246244+RoxanneLefebvre@users.noreply.github.com>
2022-04-12 15:16:23 -0400

    debugg

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 document.querySelector('#button').addEventListener('click', logMessages);
 
 function logMessages(){
+    
     console.log('Message');
     console.warn('Attention!');
     console.error('erreur');
